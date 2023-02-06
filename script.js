e250de4e2510056c89ba6f9dba8b100e25eef62e
Zahra <you@example.com>
2023-02-07 00:18:57 +0330

    generate password function added

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const passwordLbl = document.querySelector("#passwordLbl")
 const generateBtn = document.querySelector("#generateBtn")
 
+function generatePassword(){
+    return Math.floor(Math.random() * 99999999)
+    
+}
+
 generateBtn.addEventListener("click",()=>{
     generatePassword()
 })
